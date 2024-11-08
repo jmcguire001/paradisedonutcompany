7a331cc093e2e6effea115a21c4da2ab89f926f1
Jake McGuire <120243972+jmcguire001@users.noreply.github.com>
2024-11-08 04:24:08 -0700

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 sudo systemctl stop nginx
-
-# Find all processes with 'dotnet' in their command
-pids=$(pgrep -f 'dotnet')
-
-if [ -z "$pids" ]; then
-  echo "No dotnet processes found."
-  exit 0
-fi
-
-# Kill each found process
-for pid in $pids; do
-  echo "Killing process with PID $pid (dotnet)"
-  kill -SIGTERM "$pid"
-done
-
-echo "All dotnet processes have been terminated."
+sudo systemctl stop pdc.service
